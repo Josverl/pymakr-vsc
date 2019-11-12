e44feca674c9a0f68929e123e6bcbabb28cff10f
Braden Mars <bradenmars@bradenmars.me>
2019-11-11 19:31:06 -0600

    ci: Compile Modules Action
    Adds action for compiling native modules via a nodejs script. Should resolve electron runtime versions from the vscode repo, compile the modules, and harvest them via mp-download.ps1

---
+++ .github/actions/compile-modules-action/index.js
@@ -0,0 +1,103 @@
+const core = require('@actions/core');
+const Octokit = require('@octokit/rest');
+const atob = require('atob');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+const octokit = new Octokit();
+
+const repo = {
+  owner: 'microsoft',
+  repo: 'vscode'
+};
+
+const resolveElectronVersion = async tag => {
+  // Fetch .yarnrc file (contains electron target)
+  const response = await octokit.repos.getContents({
+    ...repo,
+    path: '.yarnrc',
+    ref: tag
+  });
+  // Parse from file
+  let content = atob(response.data.content).split('\n');
+  let version = content[1].split('target ')[1];
+  version = version.substring(1, version.length - 1);
+  core.info('Found electron tag: ', version);
+  return {
+    tag: tag,
+    runtime_version: version
+  };
+};
+
+const getVSCodeTags = async () => {
+  console.log('Fetching tags...');
+  const repo_tags = await octokit.repos.listTags({
+    ...repo,
+    per_page: 50
+  });
+
+  const versReg = /^([0-9]|[1-9][0-9]*)\.([0-9]|[1-9][0-9]*)\.([0-9]|[1-9][0-9]*)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?$/gm;
+
+  // Filter valid tags
+  let valid_tags = repo_tags.data.filter(i => {
+    let vers = i.name;
+    if (vers.includes('vsda') || vers.includes('translation')) {
+      return false;
+    }
+    if (versReg.test(vers)) {
+      return vers;
+    }
+  });
+  // Take 3 most recent
+  valid_tags = Array.from(valid_tags.slice(0, 3), i => i.name);
+  core.debug('Valid tags:', valid_tags);
+
+  // Prepend master tag
+  const tags = ['master', ...valid_tags];
+  return tags;
+};
+
+const execute = async command => {
+  const { stdout, stderr } = await exec(command);
+  if (stderr) {
+    core.setFailed(stderr);
+  }
+  console.log(stdout);
+  core.info(stdout);
+  return stdout;
+};
+
+const harvestModules = async runtime_version => {
+  const cmd = `pwsh ./scripts/mp-download.ps1 -harvest -runtime_version ${runtime_version}`;
+  await execute(cmd);
+};
+
+const compileModules = async ({ tag, runtime_version } = {}) => {
+  console.info(
+    `Compiling for VSCode Version: ${tag} - Electron: ${runtime_version}`
+  );
+  const cmd = `npx electron-rebuild --version ${runtime_version} --force`;
+  await execute(cmd);
+  await harvestModules(runtime_version);
+};
+
+const run = async () => {
+  try {
+    // Fetch git tags from VSCode Repo
+    const tags = await getVSCodeTags();
+    core.info('Found VSCode Tags:', tags);
+    // Resolve Electron Versions
+    const versions = await Promise.all(
+      tags.map(i => resolveElectronVersion(i))
+    );
+    core.info('Resolved versions:', versions);
+    // Compile Native Modules
+    await execute('pwd');
+    await Promise.all(versions.map(i => compileModules(i)));
+    core.setOutput('compiled', versions);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+run();
